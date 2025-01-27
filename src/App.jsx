5d87bd15875dc38bdd317113b0007bf2ae421c34
md-farhan-navid <luminous.group.336699@gmail.com>
2025-01-27 22:02:01 +0600

    added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+"use client"
 import { Button } from "@chakra-ui/react"
-
+import {ColorModeButton,useColorMode,useColorModeValue,} from "./components/ui/color-mode"
 
 function App() {
+
+  const { toggleColorMode } = useColorMode()
+
   return (
     <>
       <Button>hLW</Button>
+      <Button variant="outline" onClick={toggleColorMode}>
+      Toggle Mode
+    </Button>
     </>
   )
 }
 
 export default App
+
+
+
